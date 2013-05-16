421bfb9878ba32b0fab9be03216508cb22072945
Noda Shimpei <noda.sin@gmail.com>
2013-05-17 02:06:00 +0900

    commit SNVolumeSlider and Sample Project

---
+++ SNVolumeSliderSample/SNVolumeSliderSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SNVolumeSliderSample
+//
+//  Created by Noda Shimpei on 2013/05/17.
+//  Copyright (c) 2013年 Noda Shimpei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SNAppDelegate class]));
+    }
+}
